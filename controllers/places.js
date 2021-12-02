f118494787a4baea824acb241ea6965e68fbc716
jackroehrig <jack.roe13@gmail.com>
2021-12-02 02:02:29 -0600

    Adding places view with mock data

--- controllers/places.js
+++ controllers/places.js
@@ -6,7 +6,32 @@ const router = express.Router()
 
 // ROUTES
 router.get('/', (req, res) => {
-    res.render('places/index')
+    let places = [{
+        name: 'Draft House',
+        pic: 'https://drafthouseverona.com/wp-content/uploads/sites/82/2019/04/drafthouse_1.jpg',
+        city: 'Verona',
+        state: 'WI',
+        cuisineType: 'Bar Food/American'
+    }, {
+        name: 'Hubbard Avenue',
+        pic: 'https://cdn.usarestaurants.info/assets/uploads/490eea2f35094153bf180c66308e5876_-united-states-wisconsin-dane-county-middleton-hubbard-avenue-diner-608-831-6800htm.jpg',
+        city: 'Middleton',
+        state: 'WI',
+        cuisineType: 'Classic American Diner'
+    }, {
+        name: 'Rare Steakhouse',
+        pic: 'https://assets.simpleviewinc.com/simpleview/image/fetch/c_limit,q_75,w_1200/https://assets.simpleviewinc.com/simpleview/image/upload/crm/madison/RARE_Library00-f0efadd85056a36_f0efaf2d-5056-a36a-08e87be1e5684fed.jpg',
+        city: 'Madison',
+        state: 'WI',
+        cuisineType: 'Fancy Steakhouse'
+    }, {
+        name: "Ian's Pizza",
+        pic: 'https://ianspizza.com/wp-content/uploads/2020/09/Ians-Pizza_Garver-Feed-Mill_1200w.jpg',
+        city: 'Madison',
+        state: 'WI',
+        cuisineType: 'Pizza Place'
+    }]
+    res.render('places/index', { places })
 })
 
 
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,12 +1,21 @@
 const React = require('react')
 const Def = require('../default')
 
-function index () {
+function index (data) {
+    let placesFormatted = data.places.map((place, index) => {
+        return (
+            <div key={index}>
+                <h2>{place.name}</h2>
+                <img src={place.pic} alt={place.name}/>
+            </div>
+        )
+    })
+
     return (
         <Def>
-            <main style={{textAlign: "center"}}>
+            <main>
                 <h1>Places</h1>
-                <p>somewhere below here is where the places will be!</p>
+                {placesFormatted}
             </main>
         </Def>
     )
