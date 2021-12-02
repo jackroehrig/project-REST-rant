269dd4c06a5d19338b893634c6eb62fc8f7161bd
jackroehrig <jack.roe13@gmail.com>
2021-12-01 22:11:10 -0600

    Adding 404 page route

--- index.js
+++ index.js
@@ -15,5 +15,14 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+// Wildcard Route
+app.get('*', (req, res) => {
+    res
+        .status(404)
+        .send(`
+        <h1>404 Page</h1>
+    `)
+})
+
 // Setting localhost Port
 app.listen(PORT)
