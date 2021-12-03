6019e3cc88f7a325d5c9a63e25d021d0dd3998ae
jackroehrig <jack.roe13@gmail.com>
2021-12-03 02:18:40 -0600

    Updated styles to home and places pages

--- views/home.jsx
+++ views/home.jsx
@@ -9,15 +9,16 @@ function home () {
     return (
         <Def>
             <main>
-                <h1>HOME</h1>
+                <h1 className='display-1'>REST-RANT</h1>
                 <div>
-                    <img src="/images/cool-pic.jpg" alt="cool picture of nature" />
-                    <div>
+                    {/* <img src="/images/cool-pic.jpg" alt="cool picture of nature" /> */}
+                    {/* <div>
                         Photo by <a href='https://unsplash.com/@evannn_n'>Sanjeevan SatheesKumar</a> on <a href="https://unsplash.com/">Unsplash</a>
-                    </div>
+                    </div> */}
                 </div>
+                <p className="lead">Welcome to Rest-Rant! A place to save and comment on your favorite restaurants.</p>
                 <a href="/places">
-                    <button className="btn-primary">Places Page</button>
+                    <button className="btn btn-light">Places Page</button>
                 </a>
             </main>
         </Def>
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -9,26 +9,30 @@ const PORT = process.env.PORT
 const Def = require('../default')
 
 // VIEW
-function index (data) {
+function index(data) {
     let placesFormatted = data.places.map(place => {
         return (
-            <div className="col-sm-6" key={place.id}>
-                <h2><a href={`http://localhost:${PORT}/places/${place.id}`}>{place.name}</a></h2>
-                <p>{place.cuisineType}</p>
-                <img src={place.pictureLink} alt={place.name}/>
-                <p>Located in {place.city}, {place.state}</p>
+            <div className="card" key={place.id}>
+                <img className="card-img-top" src={place.pictureLink} alt={place.name} />
+                <div className="card-body">
+                    <h2 className="card-title"><a className="stretched-link" href={`http://localhost:${PORT}/places/${place.id}`}>{place.name}</a></h2>
+                    <p className="card-text">{place.cuisineType}</p>
+                </div>
+                <div className="card-footer">
+                    <p className="card-text">Located in {place.city}, {place.state}</p>
+                </div>
             </div>
         )
     })
 
     return (
         <Def>
-            <main>
-                <h1>Places to Rant or Rave About</h1>
+            <main className="container-fluid">
+                <h1 className="display-3">Places to Rant or Rave About</h1>
                 <a href=".">
-                    <button>Home</button>
+                    <button className="btn btn-light btn-sm">Home</button>
                 </a>
-                <div className="row">
+                <div className="card-deck">
                     {placesFormatted}
                 </div>
             </main>
