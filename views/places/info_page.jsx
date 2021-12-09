03902a5f689f067a01c14ee9177035229d670454
jackroehrig <jack.roe13@gmail.com>
2021-12-09 11:05:19 -0600

    Added styles to info-page mainly the comments

--- views/places/info_page.jsx
+++ views/places/info_page.jsx
@@ -11,6 +11,7 @@ const Def = require('../default')
 // VIEW
 function placeInfo(data){
     let placeObj = data.place
+
     let comments = (
         <h3 className="inactive">
             No comments yet!
@@ -19,14 +20,25 @@ function placeInfo(data){
 
     if(data.place.comments.length) {
         comments = data.place.comments.map((c, index) => {
+
+            let stars = []
+
+            for(let i = c.stars; i > 0; i--){
+                i == 0.5
+                ? stars.push(<i className="fas fa-star-half fa-lg" key={i}/>)
+                : stars.push(<i className="fas fa-star fa-lg" key={i}/>)
+            }
+
             return (
-                <div className="border" key={index}>
-                    <h2 className="rant">{c.rant ? 'Rant! >:(' : 'Rave! :)'}</h2>
-                    <h4>{c.content}</h4>
-                    <h3>
-                        <stong>- {c.author}</stong>
-                    </h3>
-                    <h4>Rating: {c.stars}</h4>
+                <div className="comment border border-4 w-50 m-auto rounded" key={index}>
+                    <div className="rant">{c.rant ? <i className="far fa-frown fa-3x text-danger" /> : <i className="far fa-grin-beam fa-3x text-success" />}</div>
+                    <div className="w-50 m-auto">
+                        <h4 className="stars my-4">{stars}</h4>
+                        <h4 className="lead my-4">{c.content}</h4>
+                        <h5 className="display-6">
+                            <strong>- {c.author}</strong>
+                        </h5>
+                    </div>
                 </div>
             )
         })
@@ -34,30 +46,32 @@ function placeInfo(data){
 
     return (
         <Def>
-            <div className="place-profile">
-                <div className="place-image">
-                    <img src={placeObj.pic} alt={`picture of ${placeObj.name}`}/>
-                </div>
-                <div className="place-info text-center">
-                    <h2 className="display-2 border-bottom mb-4">{placeObj.name}</h2>
-                    <h3>Rating</h3>
-                    <p>unknown</p>
-                    <h3 className="mt-3">Description</h3>
-                    <p>{placeObj.showEstablished()}</p>
-                    <p>{`Serving: ${placeObj.cuisines}`}</p>
-                    <a href={`/places/${placeObj.id}/edit`}>
-                        <button className="btn btn-warning"><i className="far fa-edit" /> Edit</button>
-                    </a>
-                    <form action={`/places/${placeObj.id}?_method=DELETE`} method="POST" style={{display: 'inline'}}>
-                        <button type='submit' className="btn btn-danger"><i className="far fa-trash-alt" /> Delete</button>
-                    </form>
-                    <a href="/places">
-                        <button className="btn btn-light">Back</button>
-                    </a>
+            <div className="place-profile container-fluid my-5">
+                <div className="row">
+                    <div className="place-image col-lg text-center">
+                        <img className="img-fluid" src={placeObj.pic} alt={`picture of ${placeObj.name}`}/>
+                    </div>
+                    <div className="place-info text-center col-lg">
+                        <h1 className="display-1 border-bottom mb-4">{placeObj.name}</h1>
+                        <h2>Rating</h2>
+                        <p>unknown</p>
+                        <h2 className="mt-3">Description</h2>
+                        <p>{placeObj.showEstablished()}</p>
+                        <p>{`Serving: ${placeObj.cuisines}`}</p>
+                        <a href={`/places/${placeObj.id}/edit`}>
+                            <button className="btn btn-dark"><i className="far fa-edit text-warning"/> Edit</button>
+                        </a>
+                        <form action={`/places/${placeObj.id}?_method=DELETE`} method="POST" style={{display: 'inline'}}>
+                            <button type='submit' className="btn btn-dark"><i className="far fa-trash-alt text-danger"/> Delete</button>
+                        </form>
+                        <a href="/places">
+                            <button className="btn btn-dark"><i className="fas fa-arrow-circle-left text-primary"/> Back</button>
+                        </a>
+                    </div>
                 </div>
             </div>
-            <div className="comments text-center border-top">
-                <h3>Comments</h3>
+            <div className="comments text-center container my-5">
+                <h2>Comments</h2>
                 {comments}
             </div>
         </Def>
